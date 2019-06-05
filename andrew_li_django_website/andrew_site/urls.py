e6b2ba9f0cfc1f6156f478ead51d35e74eab4b01
Zeyu-Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-06-05 11:50:10 -0600

    restricted user and updated templates

--- andrew_li_django_website/andrew_site/urls.py
+++ andrew_li_django_website/andrew_site/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth.views import (
 )
 from login import views as user_views
 from shop import views as shop_views
-from shop.views import ItemsView, ItemDetailView, add_to_cart,
+from shop.views import ItemsView, ItemDetailView, add_to_cart
 
 # TODO: 404 errors
 
@@ -43,8 +43,5 @@ urlpatterns = [
     # shop
     path('secret_passage/', shop_views.secret, name='secret_passage'),
     path('shop/', ItemsView.as_view(), name='item_list'),
-    path('shop/product/<slug>/', ItemDetailView.as_view(), name='product'),
-    path('shop/checkout/', shop_views.checkout, name='checkout'),
-    path('shop/add-to-cart/', shop_views.add_to_cart, name='add-to-cart'),
 
 ]
