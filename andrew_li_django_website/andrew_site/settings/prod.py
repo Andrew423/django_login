1e9a01879548ab5f9136e787d9415429029a27c6
unknown <zeyu.li23@gmail.com>
2019-06-09 22:50:18 -0600

    updated settings

--- andrew_li_django_website/andrew_site/settings/prod.py
+++ andrew_li_django_website/andrew_site/settings/prod.py
@@ -3,7 +3,7 @@ from andrew_site.settings.base import *
 # override base setting here
 DEBUG = False
 
-SECRET_KEY = 'a0oe6rx_q$6_or^+jw1zvw+uxq_t1+xn08#3&kd19p$4#hniyt'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = [
     "*"
